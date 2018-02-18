9a2b71e9efabbf620a8dc959e7e416c1570725fd
Tom J Nowell <contact@tomjn.com>
2018-02-18 20:10:03 +0000

    moves the stores initial state to a dedicated file

--- js/index.js
+++ js/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import vvvSitesApp from './reducers/reducers.js'
-import VisibleSiteList from './components/containers/visible-site-list.js';
-
+import vvvSitesApp from './reducers/reducers'
+import VisibleSiteList from './components/containers/visible-site-list';
+import { initial_state } from './reducers/initial-state';
 import {
 	ADD_SITE
-} from './reducers/actions'
+} from './reducers/actions';
 
-let store = createStore( vvvSitesApp, {
-	sites:[],
-	visibilityFilter: 'ALL'
-} );
+let store = createStore( vvvSitesApp, initial_state );
 
 // import all the sites intothe store from the config via window.vvv_sites
 Object.keys( window.vvv_sites).map( k => {
---
+++ js/reducers/initial-state.js
@@ -0,0 +1,5 @@
+
+export const initial_state = {
+	sites:[],
+	visibilityFilter: 'ALL'
+};
