28d931b2f9e43153e6c2f6145dc62b484987da91
Tom J Nowell <contact@tomjn.com>
2018-02-18 00:42:57 +0000

    fixes minor indenting in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
 	},
 	plugins:[
-	new webpack.DefinePlugin({
-		'process.env.NODE_ENV': JSON.stringify('production')
-	})
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		})
 	],
 	module: {
 		rules: [
