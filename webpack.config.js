17ce3627dcd0196cf4f9ddc5fbbdf901ca3b21b5
Tom J Nowell <contact@tomjn.com>
2018-02-19 21:38:04 +0000

    switches to the airbnb eslint rules, and adds an npm test script

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,43 @@ const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	entry: {
-		'bundle': './js/index.js',
-		"bundle.min": "./js/index.js"
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	plugins:[
-		new UglifyJsPlugin({
-			include: /\.min\.js$/
-		}),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(/*process.env.NODE_ENV ||*/ 'development')
-		}),
-		/*new webpack.DefinePlugin({
+  entry: {
+    bundle: './js/index.js',
+    'bundle.min': './js/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      include: /\.min\.js$/,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(/* process.env.NODE_ENV || */ 'development'),
+    }),
+    /* new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify('production')
-		})*/
-	],
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		mainFields: ['browser', 'main']
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components|dist)/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components|dist)/,
-				use: [ "babel-loader", 'eslint-loader']
-			}
-		]
-	}
+		}) */
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    mainFields: ['browser', 'main'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components|dist)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components|dist)/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
 };
