c398d1db9b106000ce9d5b0904878b3c78518c21
Tom J Nowell <contact@tomjn.com>
2018-06-26 13:16:28 +0100

    only show mailcatcher if it's installed, and do the same for mailhog

--- php/blocks/sidebar/bundled-tools.php
+++ php/blocks/sidebar/bundled-tools.php
@@ -4,7 +4,18 @@
 	<a class="button" href="//vvv.test/database-admin/" target="_blank">phpMyAdmin</a>
 	<a class="button" href="//vvv.test/memcached-admin/" target="_blank">phpMemcachedAdmin</a>
 	<a class="button" href="//vvv.test/opcache-status/opcache.php" target="_blank">Opcache Status</a>
-	<a class="button" href="http://vvv.test:1080" target="_blank">Mailcatcher</a>
+	<?php
+	if ( file_exists( '/usr/local/rvm/bin/mailcatcher' ) ) {
+		?>
+		<a class="button" href="http://vvv.test:1080" target="_blank">Mailcatcher</a>
+		<?php
+	}
+	if ( file_exists( '/usr/local/bin/mailhog' ) ) {
+		?>
+		<a class="button" href="http://vvv.test:8025" target="_blank">MailHog</a>
+		<?php
+	}
+	?>
 	<a class="button" href="//vvv.test/webgrind/" target="_blank">Webgrind</a>
 	<a class="button" href="//vvv.test/phpinfo/" target="_blank">PHP Info</a>
 	<a class="button" href="php-status?html&amp;full" target="_blank">PHP Status</a>
