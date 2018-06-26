3f93f553a67da55ddf63861d7e1b412ee4850874
Tom J Nowell <contact@tomjn.com>
2018-06-26 13:35:49 +0100

    Show the VVV version on the dashboard if present, closes #2

--- php/blocks/sidebar/find-out-more-vvv.php
+++ php/blocks/sidebar/find-out-more-vvv.php
@@ -1,5 +1,12 @@
 <div class="box">
 	<h3>Find out more about VVV</h3>
+	<?php
+	if ( file_exists( '/vagrant/version' ) ) {
+		?>
+		<p>You're running VVV <code><?php echo file_get_contents( '/vagrant/version' ); ?></code>, run <code>git pull</code> and reprovision to update</p>
+		<?php
+	}
+	?>
 	<a class="button" href="https://varyingvagrantvagrants.org/" target="_blank">Help &amp; Documentation</a>
 	<a class="button" href="https://github.com/varying-vagrant-vagrants/vvv/" target="_blank">View the code on GitHub</a>
 </div>
