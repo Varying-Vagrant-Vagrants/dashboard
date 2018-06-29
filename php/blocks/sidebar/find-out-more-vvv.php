21858cfb44785bb0800a076e997f845986f63cd3
Tom J Nowell <contact@tomjn.com>
2018-06-29 17:03:10 +0100

    Update find-out-more-vvv.php

--- php/blocks/sidebar/find-out-more-vvv.php
+++ php/blocks/sidebar/find-out-more-vvv.php
@@ -2,8 +2,10 @@
 	<h3>Find out more about VVV</h3>
 	<?php
 	if ( file_exists( '/vagrant/version' ) ) {
+		// note that at this time we cannot check for updates to be 100% sure,
+		// that requires a network connection which isn't always present
 		?>
-		<p>You're running VVV <code><?php echo file_get_contents( '/vagrant/version' ); ?></code>, run <code>git pull</code> and reprovision to update</p>
+		<p>You're running VVV <code><?php echo file_get_contents( '/vagrant/version' ); ?></code>, <strong>if</strong> there are updates available you can run <code>git pull</code> and reprovision to apply them</p>
 		<?php
 	}
 	?>
