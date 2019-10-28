34cf67563f3601a6918e0e9662c2f8f0b7d43e19
Tom J Nowell <contact@tomjn.com>
2019-10-28 21:01:30 +0000

    Update bundled-environments-intro.php

--- php/blocks/main/bundled-environments-intro.php
+++ php/blocks/main/bundled-environments-intro.php
@@ -1,9 +1,4 @@
 <div class="box">
 	<h3>Bundled Environments</h3>
 	<p>VVV reads a config file to discover and provision sites named in <code>config/config.yml</code>. If it doesn't exist, it falls back to <code>config/default-config.yml</code>.
-	<?php if ( ! file_exists( '/srv/config/config.yml' ) ) { ?>
-		<b><code>config.yml</code> does not exist! Please copy <code>config/default-config.yml</code> to <code>config/config.yml</code></b>
-	<?php } else { ?>
-		Below is a list of the sites in <code>config.yml</code>, remember to reprovision if you change it!
-	<?php } ?>
 </div>
