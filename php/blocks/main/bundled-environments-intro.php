3b6464536400ccd9a6cc5332c3176791b2e9fc66
Daniele Scasciafratte <mte90net@gmail.com>
2019-10-28 16:27:29 +0100

    support for #1955

--- php/blocks/main/bundled-environments-intro.php
+++ php/blocks/main/bundled-environments-intro.php
@@ -1,9 +1,9 @@
 <div class="box">
 	<h3>Bundled Environments</h3>
 	<p>VVV reads a config file to discover and provision sites named in <code>vvv-custom.yml</code>. If it doesn't exist, it falls back to <code>vvv-config.yml</code>.
-	<?php if ( ! file_exists( '/vagrant/vvv-custom.yml' ) ) { ?>
-		<b><code>vvv-custom.yml</code> does not exist! Please copy <code>vvv-config.yml</code> to <code>vvv-custom.yml</code></b>
+	<?php if ( ! file_exists( '/srv/config/config.yml' ) ) { ?>
+		<b><code>vvv-custom.yml</code> does not exist! Please copy <code>config/default-config.yml</code> to <code>config/config.yml</code></b>
 	<?php } else { ?>
-		Below is a list of the sites in <code>vvv-custom.yml</code>, remember to reprovision if you change it!
+		Below is a list of the sites in <code>config.yml</code>, remember to reprovision if you change it!
 	<?php } ?>
 </div>
