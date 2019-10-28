cfe9aaaf92a8ee75a5f7c81c1a8ca6f7f810fe24
Daniele Scasciafratte <mte90net@gmail.com>
2019-10-28 18:29:13 +0100

    missing part

--- php/blocks/main/bundled-environments-intro.php
+++ php/blocks/main/bundled-environments-intro.php
@@ -1,6 +1,6 @@
 <div class="box">
 	<h3>Bundled Environments</h3>
-	<p>VVV reads a config file to discover and provision sites named in <code>vvv-custom.yml</code>. If it doesn't exist, it falls back to <code>vvv-config.yml</code>.
+	<p>VVV reads a config file to discover and provision sites named in <code>config/config.yml</code>. If it doesn't exist, it falls back to <code>config/default-config.yml</code>.
 	<?php if ( ! file_exists( '/srv/config/config.yml' ) ) { ?>
 		<b><code>config.yml</code> does not exist! Please copy <code>config/default-config.yml</code> to <code>config/config.yml</code></b>
 	<?php } else { ?>
