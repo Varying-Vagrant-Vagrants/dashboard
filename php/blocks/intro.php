242b03c9c04f9d0b158a5fe91d0030e9b0ff3a35
tomjn <contact@tomjn.com>
2020-02-13 12:34:46 +0000

    adjust the top intro

--- php/blocks/intro.php
+++ php/blocks/intro.php
@@ -1,4 +1,3 @@
 <div class="box alt-box">
 	<p>VVV is a local web development environment powered by Vagrant and Virtual Machines.</p>
-	<p>To add, remove, or change sites, modify <code>config/config.yml</code> then reprovision using <code>vagrant reload --provision</code></p>
 </div>
--- php/blocks/main/bundled-environments-intro.php
+++ php/blocks/main/bundled-environments-intro.php
@@ -1,4 +1,5 @@
-<div class="box">
+<div class="box alt-box">
 	<h3>Bundled Environments</h3>
 	<p>VVV reads a config file to discover and provision sites named in <code>config/config.yml</code>. If it doesn't exist, it falls back to <code>config/default-config.yml</code>.
+	<p>To add, remove, or change sites, modify <code>config/config.yml</code> then reprovision using <code>vagrant reload --provision</code></p>
 </div>
