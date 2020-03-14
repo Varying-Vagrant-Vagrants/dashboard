5eb8d2226adafa3a9fea955feee93165548790e5
tomjn <contact@tomjn.com>
2020-03-14 17:41:08 +0000

    split debug ext and php versions into 2 boxes, added 5.6 support, and replaced table for extensions with a paragraph

--- php/blocks/main/php-status.php
+++ php/blocks/main/php-status.php
@@ -4,13 +4,25 @@ $version = $version[0] . "." . $version[1] . "." . explode( '+', $version[2] )[0
 ?>
 <div class="box alt-box">
 	<h3>PHP Status</h3>
-	<p>The default version of PHP is <code>v<?php echo $version; ?></code>, but individual sites can use other versions of PHP.</p>
+
+	<p>The default version of PHP is <code>v<?php echo $version; ?></code>, but individual sites can use other versions of PHP. <a href="https://launchpad.net/~ondrej/+archive/ubuntu/php" target="_blank">PHP packages are installed from Ondřej Surý PPA</a></p>
+
+	<p>Update your <code>config/config.yml</code> file to install more versions of PHP</p>
+
 
 	<table>
 		<tr>
-			<th>PHP</th>
+			<th>PHP Installations</th>
 		</tr>
 <?php
+$files = glob('/usr/bin/php5.*');
+foreach ( $files as $file ) {
+	?>
+		<tr>
+			<td><?php echo basename( $file ) ?></td>
+		</tr>
+	<?php
+}
 $files = glob('/usr/bin/php7.*');
 foreach ( $files as $file ) {
 	?>
@@ -22,24 +34,34 @@ foreach ( $files as $file ) {
 ?>
 
 	</table>
+</div>
+<div class="box alt-box">
+	<h3>PHP Debug Extension</h3>
 
-	<table>
-		<tr>
-			<th>Debug Extension</th>
-			<th>Loaded</th>
-			<th>Version</th>
-		</tr>
-		<tr>
-			<td>XDebug</td>
-			<td><?php echo extension_loaded('xdebug') ? 'yes' : 'no'; ?></td>
-			<td><?php echo phpversion( 'xdebug' ); ?></td>
-		</tr>
-		<tr>
-			<td>Tideways/XHProf</td>
-			<td><?php echo extension_loaded('tideways_xhprof') ? 'yes' : 'no'; ?></td>
-			<td><?php echo phpversion( 'tideways_xhprof' ); ?></td>
-		</tr>
-	</table>
+<?php
+$exts = [
+	'XDebug'	=> 'xdebug',
+	'Tideways'  => 'tideways_xhprof',
+	'PCov'      => 'pcov',
+];
+$current_debug_ext = '';
+foreach ( $exts as $name => $ext ) {
+	if ( ! extension_loaded( $ext ) ) {
+		continue;
+	}
+	$current_debug_ext = $ext;
+	?>
+	<p>The currently loaded debugging extension for PHP is <strong><?php echo $name; ?></strong> at version <code><?php echo phpversion( 'xdebug' ); ?></code></p>
+	<?php
+	break;
+}
+if ( empty( $current_debug_ext ) ) {
+	?>
+	<p>No debugging extension is active at the moment</p>
+	<?php
+}
+?>
 
-	<p>To switch between debug mods, use <code>vagrant ssh</code> to enter the VM, then use <code>switch_php_debugmod</code> followed by the extension you prefer. For example to use XDebug, you would run <code>switch_php_debugmod xdebug</code>. On older versions of VVV you would need to use <code>xdebug_on</code> or <code>xdebug_off</code>.</p>
+	<p>To switch between debug extensions, use <code>vagrant ssh</code> to enter the VM, then use <code>switch_php_debugmod</code> followed by the extension you prefer. For example <code>switch_php_debugmod xdebug</code>. Use <code>switch_php_debugmod none</code> to disable them. On older versions of VVV you would need to use <code>xdebug_on</code> or <code>xdebug_off</code>.</p>
+	<p>All installs come with XDebug installed but turned off. Tideways can be added by modifying <code>config/config.yml</code></p>
 </div>
