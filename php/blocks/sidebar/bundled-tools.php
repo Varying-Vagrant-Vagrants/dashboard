eea815375cde8ed85f69cf8c669a51dbccbe159b
Daniele Scasciafratte <mte90net@gmail.com>
2020-10-28 13:10:32 +0100

    replace utulity detection for sidebar

--- php/blocks/sidebar/bundled-tools.php
+++ php/blocks/sidebar/bundled-tools.php
@@ -1,37 +1,39 @@
 <div class="box">
-	<h3>Tools</h3>
-	<nav>
-		<?php
-		if ( is_dir( '/srv/www/default/database-admin/' ) ) {
-			?><a class="button" href="//vvv.test/database-admin/" target="_blank">phpMyAdmin</a> <?php
-		}
-
-		if ( is_dir( '/srv/www/default/memcached-admin/' ) ) {
-			?><a class="button" href="//vvv.test/memcached-admin/" target="_blank">phpMemcachedAdmin</a> <?php
-		}
-
-		if ( is_dir( '/srv/www/default/opcache-status/' ) ) {
-			?><a class="button" href="//vvv.test/opcache-status/opcache.php" target="_blank">Opcache Status</a> <?php
-		}
-
-		if ( is_dir( '/srv/www/default/opcache-gui/' ) ) {
-			?><a class="button" href="//vvv.test/opcache-gui/" target="_blank">Opcache Gui</a> <?php
+    <h3>Tools</h3>
+    <nav>
+        <?php
+        $utilities = get_config_yaml();
+        if ( in_array( 'phpmyadmin', $utilities['utilities']['core'] ) ) {
+            ?><a class="button" href="//vvv.test/database-admin/" target="_blank">phpMyAdmin</a> <?php
         }
 
-		if ( file_exists( '/usr/local/bin/mailhog' ) ) {
-			?><a class="button" href="http://vvv.test:8025" target="_blank">📨 MailHog</a> <?php
-		} else if ( file_exists( '/usr/local/rvm/bin/mailcatcher' ) ) {
-			?><a class="button" href="http://vvv.test:1080" target="_blank">📨 Mailcatcher</a> <?php
-		}
+        if ( in_array( 'memcached-admin', $utilities['utilities']['core'] ) ) {
+            ?><a class="button" href="//vvv.test/memcached-admin/" target="_blank">phpMemcachedAdmin</a> <?php
+        }
 
-		if ( is_dir( '/srv/www/default/xhgui/' ) ) {
-			?><a class="button" href="//xhgui.vvv.test/" target="_blank">XHGui Profiler</a> <?php
-		}
-		if ( is_dir( '/srv/www/default/webgrind/' ) ) {
-			?><a class="button" href="//vvv.test/webgrind/" target="_blank">Webgrind</a> <?php
-		}
-		?>
-		<a class="button" href="//vvv.test/phpinfo/" target="_blank">PHP Info</a>
-		<a class="button" href="php-status?html&amp;full" target="_blank">PHP Status</a>
-	</nav>
+        if ( in_array( 'opcache-status', $utilities['utilities']['core'] ) ) {
+            ?><a class="button" href="//vvv.test/opcache-status/opcache.php" target="_blank">Opcache Status</a> <?php
+        }
+
+        if ( in_array( 'opcache-gui', $utilities['utilities']['core'] ) ) {
+            ?><a class="button" href="//vvv.test/opcache-gui/" target="_blank">Opcache Gui</a> <?php
+        }
+
+        if ( file_exists( '/usr/local/bin/mailhog' ) ) {
+            ?><a class="button" href="http://vvv.test:8025" target="_blank">📨 MailHog</a> <?php
+        } else if ( file_exists( '/usr/local/rvm/bin/mailcatcher' ) ) {
+            ?><a class="button" href="http://vvv.test:1080" target="_blank">📨 Mailcatcher</a> <?php
+        }
+
+        if ( in_array( 'tideways', $utilities['utilities']['core'] ) ) {
+            ?><a class="button" href="//xhgui.vvv.test/" target="_blank">XHGui Profiler</a> <?php
+        }
+        if ( in_array( 'webgrind', $utilities['utilities']['core'] ) ) {
+            ?><a class="button" href="//vvv.test/webgrind/" target="_blank">Webgrind</a> <?php
+        }
+        ?>
+        <a class="button" href="//vvv.test/phpinfo/" target="_blank">PHP Info</a>
+        <a class="button" href="php-status?html&amp;full" target="_blank">PHP Status</a>
+    </nav>
 </div>
+
