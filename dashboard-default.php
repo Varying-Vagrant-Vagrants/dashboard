2aef18551c6647582c416c730b03479277bf4247
tomjn <contact@tomjn.com>
2020-12-04 18:42:44 +0000

    if the VM has the root provisioned flag set, make a big song and dance about it and warn the user

--- dashboard-default.php
+++ dashboard-default.php
@@ -1,16 +1,15 @@
 <?php
-$version = file_get_contents('/vagrant/version');
-
+$version = file_get_contents( '/vagrant/version' );
+$root_warning = file_exists( '/vagrant/provisioned_as_root' );
 
 ?><!DOCTYPE html>
 <html>
 	<head>
 		<title>VVV Dashboard</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" type="text/css" href="//vvv.test/dashboard/style.css?t=<?php echo intval( filemtime( __DIR__.'/style.css' ) ); ?>">
+		<link rel="stylesheet" type="text/css" href="//vvv.test/dashboard/style.css?t=<?php echo intval( filemtime( __DIR__ . '/style.css' ) ); ?>">
 	</head>
-	<body>
-		
+	<body class="<?php echo $root_warning ? 'root-warning' : ''; ?>">
 		<header>
 			<h2 id="vvv_logo">
 				<img src="//vvv.test/dashboard/vvv-tight.png"/> VVV v<span class="version"><?php echo strip_tags( $version ); ?></span>
@@ -18,22 +17,21 @@ $version = file_get_contents('/vagrant/version');
 		</header>
 		<?php
 		require_once( __DIR__ . '/php/notices.php' );
-		//require_once( __DIR__ . '/php/blocks/intro.php' );
 		?>
 		<div class="grid">
 			<main class="column left-column">
 				<?php
-				require_once( __DIR__ . '/php/blocks/main/sites.php' );
-				require_once( __DIR__ . '/php/blocks/main/adding-site-doc.php' );
-				require_once( __DIR__ . '/php/blocks/main/php-status.php' );
+				require_once __DIR__ . '/php/blocks/main/sites.php';
+				require_once __DIR__ . '/php/blocks/main/adding-site-doc.php';
+				require_once __DIR__ . '/php/blocks/main/php-status.php';
 				?>
 			</main>
 			<aside class="column right-column">
 				<?php
-				require_once( __DIR__ . '/php/blocks/sidebar/inclusivity.php' );
-				require_once( __DIR__ . '/php/blocks/sidebar/bundled-tools.php' );
-				require_once( __DIR__ . '/php/blocks/sidebar/search-docs.php' );
-				require_once( __DIR__ . '/php/blocks/sidebar/find-out-more-vvv.php' );
+				require_once __DIR__ . '/php/blocks/sidebar/inclusivity.php';
+				require_once __DIR__ . '/php/blocks/sidebar/bundled-tools.php';
+				require_once __DIR__ . '/php/blocks/sidebar/search-docs.php';
+				require_once __DIR__ . '/php/blocks/sidebar/find-out-more-vvv.php';
 				?>
 			</aside>
 		</div>
--- php/blocks/sidebar/inclusivity.php
+++ php/blocks/sidebar/inclusivity.php
@@ -1,13 +1,29 @@
+<?php
+if ( file_exists( '/vagrant/provisioned_as_root' ) ) {
+	?>
+<div class="box inclusivity root-bear">
+	<div class="inclusive-teddy-bear"><h3>sudo vagrant up</h3>
+<pre class="teddy"> ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄
+ █▒▒░░░░░░░░░▒▒█
+  █░░█░░░░░█░░█ 
+   █░:░▀█▀░:░█  
+ ▄▄▀▄:░░░░░:▄▀▄▄</pre>
+	</div>
+</div>
+<?php
+	return;
+}
+?>
 <div class="box inclusivity">
-<div class="inclusive-teddy-bear">
-	<h3>Black Lives Matter!</h3> 
+	<div class="inclusive-teddy-bear">
+		<h3>Black Lives Matter!</h3>
 
 <pre class="teddy"> ✧ ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄ ✧ 
   ✧█▒▒░░░░░░░░░▒▒█   
 ✧   █░░█░░░░░█░░█ ✧  
  ▄▄  █░░░▀█▀░░░█  ▄▄✧
 █░░█ ▀▄░░░░░░░▄▀ █░░█</pre>
-</div>
+	</div>
 	<ul>
 		<li>
 			<a href="https://blacklivesmatters.carrd.co/" target="_blank">
