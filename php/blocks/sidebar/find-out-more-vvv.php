0ee3ef83a53ee7c8361845fb8e1abcc6a15e39e8
Tom J Nowell <contact@tomjn.com>
2023-02-19 15:35:34 +0000

    mention the linux distro in the dashboard

--- php/blocks/sidebar/find-out-more-vvv.php
+++ php/blocks/sidebar/find-out-more-vvv.php
@@ -5,9 +5,11 @@
 		// note that at this time we cannot check for updates to be 100% sure,
 		// that requires a network connection which isn't always present
 		?>
-		<p>You're running VVV <code><?php echo trim( strip_tags( file_get_contents( '/vagrant/version' ) ) ); ?></code>, <strong>if</strong> there are updates available you can run <code>git pull</code> and reprovision to apply them</p>
+		<p>You're running VVV <code>v<?php echo trim( strip_tags( file_get_contents( '/vagrant/version' ) ) ); ?></code>, <strong>if</strong> there are updates available you can run <code>git pull</code> and <code>vagrant up --provision</code> to apply them.</p>
 		<?php
 	}
+	$distro_version = exec( 'lsb_release -ds' );
 	?>
+	<p>This VVV instance is inside a <?php echo $distro_version; ?> virtual machine.</p>
 	<a class="button" href="https://github.com/varying-vagrant-vagrants/vvv/" target="_blank">View the code on GitHub</a>
 </div>
