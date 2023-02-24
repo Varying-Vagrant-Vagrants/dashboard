a55582eb291a3d2530681056e7e2c107ccb8684f
Tom J Nowell <contact@tomjn.com>
2023-02-24 08:22:02 +0000

    replaced a vagrant reload with a vagrant up in the adding sites box

--- php/blocks/main/adding-site-doc.php
+++ php/blocks/main/adding-site-doc.php
@@ -1,3 +1,3 @@
 <div class="box alt-box">
-	<p>Want more sites? Read <a href="https://varyingvagrantvagrants.org/docs/en-US/adding-a-new-site/">how to add a new site</a> or <a href="https://varyingvagrantvagrants.org/docs/en-US/adding-a-new-site/adding-an-existing-site/">how to add existing sites</a>. To disable a site just remove its entry from <code>config/config.yml</code> then reprovision with <code>vagrant reload --provision</code>. You can also set <code>skip_provisioning: true</code></p>
+	<p>Want more sites? Read <a href="https://varyingvagrantvagrants.org/docs/en-US/adding-a-new-site/">how to add a new site</a> or <a href="https://varyingvagrantvagrants.org/docs/en-US/adding-a-new-site/adding-an-existing-site/">how to add existing sites</a>. To disable a site just remove its entry from <code>config/config.yml</code> then reprovision with <code>vagrant up --provision</code>. You can also set <code>skip_provisioning: true</code></p>
 </div>
