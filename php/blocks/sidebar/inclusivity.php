b6697b9df832779864bf6002223e8ff2b7a6a334
Tom J Nowell <contact@tomjn.com>
2025-08-24 18:07:53 +0100

    Simplify markup a little

--- php/blocks/sidebar/inclusivity.php
+++ php/blocks/sidebar/inclusivity.php
@@ -5,8 +5,8 @@ if ( file_exists( '/vagrant/provisioned_as_root' ) ) {
 	<div class="inclusive-teddy-bear"><h3>sudo vagrant up</h3>
 <pre class="teddy"> ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄
  █▒▒░░░░░░░░░▒▒█
-  █░░█░░░░░█░░█ 
-   █░:░▀█▀░:░█  
+  █░░█░░░░░█░░█
+   █░:░▀█▀░:░█
  ▄▄▀▄:░░░░░:▄▀▄▄</pre>
 	</div>
 </div>
@@ -14,15 +14,15 @@ if ( file_exists( '/vagrant/provisioned_as_root' ) ) {
 	return;
 }
 ?>
-<div class="box inclusivity" style="background: #21242c;">
+<div class="box inclusivity">
 	<div class="inclusive-teddy-bear">
 		<h3>Ukraine needs your help</h3>
 
-<pre class="teddy" style="color: #ffd700;"> ! ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄ ! 
-  !█▒▒░░░░░░░░░▒▒█   
-!   █░░█░░░░░█░░█ !  
+<pre class="teddy" style="color: #ffd700;"> ! ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄ !
+  !█▒▒░░░░░░░░░▒▒█
+!   █░░█░░░░░█░░█ !
      █░░░▀█▀░░░█    !
-  !  ▀▄░░░░░░░▄▀     
+  !  ▀▄░░░░░░░▄▀
        ▀▀▀▀▀▀▀  !    </pre>
 	</div>
 	<ul>
